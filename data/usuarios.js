f5de40c95d4ffd00ffb03af2f9900fd9ece62f40
DubanPrograDVR <156736859+DubanPrograDVR@users.noreply.github.com>
2025-10-16 13:11:04 -0300

    Creacion de login y se agrego el un logo

---
+++ data/usuarios.js
@@ -0,0 +1,7 @@
+export const usuarios = [
+  {
+    id: 1,
+    usuario: "Naxre09",
+    contraseña: "1234",
+  },
+];
--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
 import { habitos } from "../data/data";
+import { usuarios } from "../data/usuarios";
 
 const agregar_habito = () => {};
+
+const validar_sesion = () => {};
